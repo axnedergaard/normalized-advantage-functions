Add covariance matrix parameterisation to NAF agent. Handle NaN action bug by terminating agent.

experiment.py
@@ -1,3 +1,5 @@
+#TODO improved logging, include if env solved
+
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
@@ -31,7 +33,12 @@ parser.add_argument('--memory_capacity', dest='memory_capacity', type=int, nargs
 parser.add_argument('-v', action='count', default=0) 
 parser.add_argument('--load', dest='load_path', type=str, default=None)
 parser.add_argument('--output', dest='output_path', type=str, default=None)
+parser.add_argument('--covariance', dest='covariance', type=str, nargs='+', default="original")
+parser.add_argument('--solve_threshold', dest='solve_threshold', type=float, nargs='+', default=None) #threshold for having solved environment
 args = parser.parse_args()
+
+def fill_episodes(rewards, n, value):
+  return rewards + [value]*n
 
 #get legends for plot
 def recursive_legend(keys, remaining_vals, vals):
@@ -68,10 +75,19 @@ def experiment(args):
   
   experiments_rewards = []
   for i in range(args['repeats']):
-    agent = naf.Agent(args['v'], env.observation_space, env.action_space, args['learning_rate'], args['batch_normalize'], args['gamma'], args['tau'], args['epsilon'], args['hidden_size'], args['hidden_n'], args['hidden_activation'], args['batch_size'], args['memory_capacity'], args['load_path'])
+    agent = naf.Agent(args['v'], env.observation_space, env.action_space, args['learning_rate'], args['batch_normalize'], args['gamma'], args['tau'], args['epsilon'], args['hidden_size'], args['hidden_n'], args['hidden_activation'], args['batch_size'], args['memory_capacity'], args['load_path'], args['covariance'])
     experiment_rewards = []
-  
+    terminate = None
+    solved = 0 #only relevant if solved_threshold is set
+
     for j in range(args['episodes']):
+      if terminate is not None:
+        fill_value = 0
+        if terminate == "solved":
+          fill_value = args['solve_threshold']
+        experiment_rewards = fill_episodes(experiment_rewards, args['episodes']-j, fill_value)
+        break
+
       rewards = 0
       state = env.reset()
  
@@ -80,6 +96,14 @@ def experiment(args):
           env.render()
         
         action = agent.get_action(state)
+        if np.isnan(np.min(action)): #if NaN action (neural network exploded)
+          print("Warning: NaN action, terminating agent")
+          with open("error.txt", "a") as error_file:
+            error_file.write(str(args) + " repeat " + str(i) + " episode " + str(j) + " step " + str(k) + " NaN\n")
+          rewards = 0 #TODO ?
+          terminate = "nan"
+          break
+        #print(action)
         state_next,reward,terminal,_ = env.step(agent.scale(action, env.action_space.low, env.action_space.high))
         
         if k-1 >= args['max_episode_steps']:
@@ -96,6 +120,16 @@ def experiment(args):
           agent.reset()
           break
       experiment_rewards += [rewards]
+
+      if args['solve_threshold'] is not None:
+        if rewards >= args['solve_threshold']:
+          solved += 1
+        else:
+          solved = 0
+        if solved >= 10: #number of repeated rewards above threshold to consider environment solved = 10
+          print("[Solved]")
+          terminate = "solved"
+
       if args['v'] > 0:
         print("Reward(" + str(i) + "," + str(j) + "," + str(k) + ")=" + str(rewards))
     if args['v'] > 1:
@@ -108,8 +142,8 @@ def experiment(args):
 #main
 
 #construct experiment inputs
-keys=['v', 'graph','render','environment','repeats','episodes','max_episode_steps','train_steps','batch_normalize', 'learning_rate','gamma','tau','epsilon','hidden_size','hidden_n','hidden_activation','batch_size', 'memory_capacity', 'load_path']
-vals=[args.v, args.graph, args.render, args.environment, args.repeats, args.episodes, args.max_episode_steps, args.train_steps, args.batch_normalize, args.learning_rate, args.gamma, args.tau, args.epsilon, args.hidden_size, args.hidden_n, args.hidden_activation, args.batch_size, args.memory_capacity, args.load_path]
+keys=['v', 'graph','render','environment','repeats','episodes','max_episode_steps','train_steps','batch_normalize', 'learning_rate','gamma','tau','epsilon','hidden_size','hidden_n','hidden_activation','batch_size', 'memory_capacity', 'load_path', 'covariance', 'solve_threshold']
+vals=[args.v, args.graph, args.render, args.environment, args.repeats, args.episodes, args.max_episode_steps, args.train_steps, args.batch_normalize, args.learning_rate, args.gamma, args.tau, args.epsilon, args.hidden_size, args.hidden_n, args.hidden_activation, args.batch_size, args.memory_capacity, args.load_path, args.covariance, args.solve_threshold]
 
 #run experiments
 rewards = recursive_experiment(keys, vals, [])
@@ -132,7 +166,7 @@ if args.output_path is not None:
   result_file.close()
 
 #plot
-if args.graph:
+if args.graph: #TODO separate plots for different envs - subplots or completely separate?
   for r in np.mean(rewards, -2): #mean of repeats
     plt.plot(r,'o')
   plt.legend(legend)

naf.py
@@ -1,12 +1,14 @@
 #Based on 'Continuous Deep Q-learning with Model Based Acceleration' by Gu et al, 2016. Available from: https://arxiv.org/pdf/1603.00748.pdf
 
 #TODO
+#investigate and fix nan action bug
 #confirm saver working
 #additional observation/action space support (continuous)
 #experiment with different advantage functions/covariance matrices for advantage function
 #more clever exploration policy
 #adaptive batch size?
 #memories weighted by information/loss?
+#improved network initialisation?
 
 import tensorflow as tf
 import numpy as np
@@ -73,7 +75,7 @@ class Layer:
     return update
 
 class Agent:
-  def __init__(self, v, observation_space, action_space, learning_rate, batch_normalize, gamma, tau, epsilon, hidden_size, hidden_n, hidden_activation, batch_size, memory_capacity, load_path):
+  def __init__(self, v, observation_space, action_space, learning_rate, batch_normalize, gamma, tau, epsilon, hidden_size, hidden_n, hidden_activation, batch_size, memory_capacity, load_path, covariance):
     self.v = v
 
     self.memory = Memory(memory_capacity,batch_size,v)
@@ -115,21 +117,25 @@ class Agent:
     self.t_V = Layer(self.t_H.h, V_n, batch_normalize=batch_normalize) #target
     self.updates += self.t_V.construct_update(self.V, self.tau)
     self.mu = Layer(self.H.h, mu_n, activation=tf.nn.tanh, batch_normalize=batch_normalize)
-    
-    #self.M = Layer(self.H.h, M_n, batch_normalize=batch_normalize)
-    #self.N = fill_lower_triangular(self.M.h)
-    #self.L = tf.matrix_set_diag(self.N, tf.exp(tf.matrix_diag_part(self.N)))
-    #self.O = Layer(self.H.h, mu_n, batch_normalize=batch_normalize) #nn input to diagonal covariance
 
-    #self.P = tf.matrix_set_diag(tf.eye(self.action_n,batch_shape=[tf.shape(self.x)[0]]), self.O.h) #diagonal covariance
-    self.P = tf.eye(self.action_n,batch_shape=[tf.shape(self.x)[0]]) #identity covariance
-    #self.P = tf.matmul(self.L, tf.matrix_transpose(self.L)) #original NAF covariance
+    if covariance == "identity": #identity covariance
+      self.P = tf.eye(self.action_n,batch_shape=[tf.shape(self.x)[0]]) #identity covariance
+
+    elif covariance == "diagonal": #diagonal covariance with nn inputs
+      self.O = Layer(self.H.h, mu_n, batch_normalize=batch_normalize) #nn input to diagonal covariance
+      self.P = tf.matrix_set_diag(tf.eye(self.action_n,batch_shape=[tf.shape(self.x)[0]]), self.O.h) #diagonal covariance
+
+    else:  #original NAF covariance
+      self.M = Layer(self.H.h, M_n, activation=tf.nn.tanh, batch_normalize=batch_normalize)
+      self.N = fill_lower_triangular(self.M.h)
+      self.L = tf.matrix_set_diag(self.N, tf.exp(tf.matrix_diag_part(self.N)))
+      self.P = tf.matmul(self.L, tf.matrix_transpose(self.L)) #original NAF covariance
+
     #self.P_inverse = tf.matrix_inverse(self.P) #precision matrix for exploration policy
     
     self.D = tf.reshape(self.u - self.mu.h, [-1,1,self.action_n])
     
     self.A =  (-1.0/2.0)*tf.reshape(tf.matmul(tf.matmul(self.D, self.P), tf.transpose(self.D, perm=[0,2,1])), [-1,1]) #advantage function
-    #self.A = -tf.reshape(tf.matmul(self.D, tf.transpose(self.D, perm=[0,2,1])), [-1,1])
     
     self.Q = self.A + self.V.h
     self.loss = tf.reduce_sum(tf.square(self.target - self.Q))
@@ -159,8 +165,8 @@ class Agent:
     self.saver.save(self.sess, path)
 
   def get_action(self,s):
-    mu = self.sess.run(self.mu.h,feed_dict={self.x:np.reshape(s,[1,-1])})[0]
-  
+    mu = self.sess.run(self.mu.h, feed_dict={self.x:np.reshape(s,[1,-1])})[0]
+    
     #random action with probability epsilon
     if np.random.rand() < self.epsilon:
       action = np.random.rand(self.action_n)*2-1 #random action